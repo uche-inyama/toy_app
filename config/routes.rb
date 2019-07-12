4d42f69d32d22d769b79322a83f280658655a204
uche-inyama <depoulet@yahoo.com>
2019-07-12 11:08:37 +0100

    Finish toy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
+end
+
+Rails.application.routes.draw do
+  resources :microposts
+  resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#index'
 end
